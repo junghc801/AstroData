0fa8db3e9378a327885e265bcee3354991334b01
junghc801 <junghc.801@gmail.com>
2024-09-02 16:15:08 -0400

    Update Week_2.py

--- Week_2.py
+++ Week_2.py
@@ -2,20 +2,37 @@ import astropy.units as u
 import astropy.constants as const
 import numpy as np
 
-# calculating orbital velocity of Earth around the Sun
-G = const.G       # G as the gravitation constant
+'''
+Calcualte the orbital velocity
+@intput: mass, radius
+@output: orbital velocity
+'''
+def orbital_velocity(mass, radius):
+  G = const.G   # G as the gravitation constant
+  v = np.sqrt(G * mass / radius)
+  return v
+  
+
+'''calculating the orbital velocity of Earth around the Sun'''  
 M = 1 * u.solMass # physical qunatity with a value and a unit.
 R = 1 * u.au      # radius of the Earth's orbit around the Sun in astronomical unit
-
-
-V = np.sqrt(G * M / R)  # 
+V = orbita_velocity(M, R) 
 print(V.to(u.km/u.s))
 
-# calculating orbital velocity of a satellite around the Earth
-
-G = const.G
+'''calculating the orbital velocity of a satellite around the Earth'''
 M_Earth = 1 * u.earthMAss
 R_Satellite = 1 * u.earthRad
-
-V_Satellite = np.sqrt(G * M_Earth / R_Satellite)
+V_Satellite = orbital_velocity(M_Earth, R_Satellite)
 print(V_Satellite.to(u.km/u.s))
+
+'''calculating the Schwarzschild radius of a solar-mass blackhole'''
+M = 1 * u.solMass
+R_Schwarzschild = 2 * G * M / (const.c**2)
+print(R_Schwarzschild.to(u.km))
+
+'''calculating the rotation curve of a galaxy with only one central super-massive blackhole'''
+mass_Black_Hole = 4.297e6 * u.solMass   # mass of the black hole
+R_Array = np.arange(1, 30) * u.au       # array of numbers from 1 to 30
+orbitalVelocities_Black_Hole = orbital_velocity(mass_Black_Hole, R_Array)
+print(orbitalVelocities_Black_Hole.to(u.km/u.s))
+
